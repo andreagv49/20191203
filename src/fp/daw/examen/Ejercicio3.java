62cb2f8a3a81e8a731feb6562b367b240150f7af
Andrea Gutierrez <alumno@a209p15.informatica.cifpaviles.pa>
2019-12-03 17:31:52 +0100

    Ejercicio 3 del examen resuelto

--- src/fp/daw/examen/Ejercicio3.java
+++ src/fp/daw/examen/Ejercicio3.java
@@ -1,5 +1,7 @@
 package fp.daw.examen;
 
+import java.util.Scanner;
+
 public class Ejercicio3 {
 
 	/* 3 puntos
@@ -13,6 +15,43 @@ public class Ejercicio3 {
 	 */
 	
 	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int numero, suma = 0, seguir = 1;
+		
+		do {
+			
+			System.out.println("Introduzca un numero entero positivo: ");
+			numero = scan.nextInt();
+			
+			for (int i = (numero-1) ; i>0 ; i--) {
+				//System.out.println(i);
+				suma+=i;
+				
+				if(suma>numero) {
+					suma=i;
+					//System.out.println("suma = "+suma);
+				}
+				
+				if(numero == suma) {
+					System.out.println("El número "+numero+" es guay");
+					break;
+				}
+			}
+			
+			if(numero != suma) {
+				System.out.println("El número "+numero+" no es guay");
+			}
+			
+			System.out.println("¿Quieres introducir otro número? Escribe 1 para SI o 0 para NO");
+			seguir = scan.nextInt();
+		
+		}while (seguir == 1);
+		
+		if(seguir == 0) {
+			System.out.println("Fin de programa");
+		}
+		scan.close();
 		
 	}
 
