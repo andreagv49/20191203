a7ca0fbdbe430901ec1933b5f1074bfac7047620
Andrea Gutierrez <alumno@a209p15.informatica.cifpaviles.pa>
2019-12-03 17:07:27 +0100

    Ejercicio 1 del examen resuelto

--- src/fp/daw/examen/Ejercicio1.java
+++ src/fp/daw/examen/Ejercicio1.java
@@ -1,5 +1,7 @@
 package fp.daw.examen;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
 	/* 
@@ -32,6 +34,146 @@ public class Ejercicio1 {
 
 	public static void main(String[] args) {
 		
+		int numero, unidad, decena, centena, millar, seguir=1;
+		String unidadRom="", decenaRom="", centenaRom="", millarRom="";
+		
+		Scanner scan = new Scanner(System.in);
+		
+		do {
+			
+			do {
+				System.out.println("Introduzca un número decimal menor o igual que 3000");
+				numero = scan.nextInt();
+				
+				if(numero > 3000) {
+					System.out.println("Número no válido");
+				}
+			
+			} while (numero > 3000);
+			
+			millar = numero/1000;
+			switch(millar) {
+				case 1:
+					millarRom = "M";
+					break;
+				case 2:
+					millarRom = "MM";
+					break;
+				case 3:
+					millarRom = "MMM";
+					break;
+			}
+			
+			centena = numero/100;
+			centena = centena%10;
+			
+			switch(centena) {
+				case 1:
+					centenaRom = "C";
+					break;
+				case 2:
+					centenaRom = "CC";
+					break;
+				case 3:
+					centenaRom = "CCC";
+					break;
+				case 4:
+					centenaRom = "CD";
+					break;
+				case 5:
+					centenaRom = "D";
+					break;
+				case 6:
+					centenaRom = "DC";
+					break;
+				case 7:
+					centenaRom = "DCC";
+					break;
+				case 8:
+					centenaRom = "DCCC";
+					break;
+				case 9:
+					centenaRom = "CM";
+					break;
+			}
+			
+			decena = numero/10;
+			decena = decena%10;
+			
+			switch(decena) {
+				case 1:
+					decenaRom = "X";
+					break;
+				case 2:
+					decenaRom = "XX";
+					break;
+				case 3:
+					decenaRom = "XXX";
+					break;
+				case 4:
+					decenaRom = "XL";
+					break;
+				case 5:
+					decenaRom = "L";
+					break;
+				case 6:
+					decenaRom = "LX";
+					break;
+				case 7:
+					decenaRom = "LXX";
+					break;
+				case 8:
+					decenaRom = "LXXX";
+					break;
+				case 9:
+					decenaRom = "XC";
+					break;
+			}
+			
+			unidad = numero%10;
+			
+			switch(unidad) {
+			case 1:
+				unidadRom = "I";
+				break;
+			case 2:
+				unidadRom = "II";
+				break;
+			case 3:
+				unidadRom = "III";
+				break;
+			case 4:
+				unidadRom = "IV";
+				break;
+			case 5:
+				unidadRom = "V";
+				break;
+			case 6:
+				unidadRom = "VI";
+				break;
+			case 7:
+				unidadRom = "VII";
+				break;
+			case 8:
+				unidadRom = "VIII";
+				break;
+			case 9:
+				unidadRom = "IX";
+				break;
+			}
+			
+			System.out.println("El número " + numero + " en romano es: " + millarRom + " " + centenaRom + " " + decenaRom + " " + unidadRom);
+			System.out.println("¿Quieres volver a introducir un número? Escribe 1 para SI o 0 para NO");
+			seguir = scan.nextInt();
+			
+		
+		}while (seguir == 1);
+		
+		if(seguir == 0) {
+			System.out.println("Fin de programa");
+		}
+		
+		scan.close();
 	}
 	
 }
